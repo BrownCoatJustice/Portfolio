5b9515486fff50f3d95d7890a081f0405cc1c749
TheSocialist Mind <brownforsocialmedia@gmail.com>
2022-04-27 20:21:16 +0530

    Changed JS File to TS file

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+let splash = ["Made out of pure CSS...*puts gun in mouth*", "Her: Centering a div is easy. Me: *Vietnam War Memories*", "Why would you come here?", "I was made in 2022", ""];
+//              0                       1                       2                                                           3                           4
+let decider = randomNumber(0,4) 
+
+
+
+function randomNumber(min, max){
+    const r = Math.random()*(max-min) + min
+    return Math.floor(r)
+}
+//Why doesnt JS provide random number function through a specified range? Should a stuck with Java.
+
